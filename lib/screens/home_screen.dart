2547aaf9047441ec4494168e849ba9c30a7cc45d
BarGhash17 <mbarghash66@gmail.com>
2024-05-20 15:53:15 +0500

    commit 5

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,85 @@
 import 'package:flutter/material.dart';
 import 'drawer.dart';
 import 'app_bar.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
+  const HomeScreen(
+      {this.currentLocationWeather,
+      this.LocationWeather,
+      this.cityName,
+      super.key});
+  final currentLocationWeather;
+  final LocationWeather;
+  final cityName;
   @override
   HomeScreenState createState() => HomeScreenState();
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  late int temperature;
+  late int maxTemperature,
+      maxTemperatureDay1,
+      maxTemperatureDay2,
+      maxTemperatureDay3;
+  late int minTemperature,
+      minTemperatureDay1,
+      minTemperatureDay2,
+      minTemperatureDay3;
+  late String city;
+  late var day1, day2, day3;
+  var date = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+
+    updateUI(
+        widget.currentLocationWeather, widget.LocationWeather, widget.cityName);
+  }
+
+  void updateUI(
+      dynamic currentWeatherData, dynamic weatherData, String cityName) {
+    city = cityName;
+    temperature =
+        currentWeatherData[0]['Temperature']['Metric']['Value'].toInt();
+    maxTemperature = weatherData['DailyForecasts'][0]['Temperature']['Maximum']
+            ['Value']
+        .toInt();
+    minTemperature = weatherData['DailyForecasts'][0]['Temperature']['Minimum']
+            ['Value']
+        .toInt();
+    day1 = DateTime.parse(weatherData['DailyForecasts'][1]['Date']);
+    day2 = DateTime.parse(weatherData['DailyForecasts'][2]['Date']);
+    day3 = DateTime.parse(weatherData['DailyForecasts'][3]['Date']);
+    maxTemperatureDay1 = weatherData['DailyForecasts'][1]['Temperature']
+            ['Maximum']['Value']
+        .toInt();
+    minTemperatureDay1 = weatherData['DailyForecasts'][1]['Temperature']
+            ['Minimum']['Value']
+        .toInt();
+    maxTemperatureDay2 = weatherData['DailyForecasts'][2]['Temperature']
+            ['Maximum']['Value']
+        .toInt();
+    minTemperatureDay2 = weatherData['DailyForecasts'][2]['Temperature']
+            ['Minimum']['Value']
+        .toInt();
+    maxTemperatureDay3 = weatherData['DailyForecasts'][3]['Temperature']
+            ['Maximum']['Value']
+        .toInt();
+    minTemperatureDay3 = weatherData['DailyForecasts'][3]['Temperature']
+            ['Minimum']['Value']
+        .toInt();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue[600],
       appBar: myAppBar(),
       drawer: const MyDrawer(),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -25,8 +87,8 @@ class HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "CHELYABINSK",
-                  style: TextStyle(
+                  city.toUpperCase(),
+                  style: const TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
@@ -37,18 +99,18 @@ class HomeScreenState extends State<HomeScreen> {
                   textBaseline: TextBaseline.alphabetic,
                   children: [
                     Text(
-                      "22°",
-                      style: TextStyle(
+                      '$temperature°',
+                      style: const TextStyle(
                         fontSize: 80,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.only(right: 10, left: 10),
+                      padding: const EdgeInsets.only(right: 10, left: 10),
                       child: Text(
-                        "Monday",
-                        style: TextStyle(
+                        DateFormat('EEEE').format(date),
+                        style: const TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -56,8 +118,8 @@ class HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     Text(
-                      "24°/12°",
-                      style: TextStyle(
+                      "$maxTemperature°/$minTemperature°",
+                      style: const TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
@@ -67,7 +129,7 @@ class HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 200,
               child: Icon(
                 Icons.sunny,
@@ -93,31 +155,11 @@ class HomeScreenState extends State<HomeScreen> {
                             color: Colors.yellow,
                           ),
                           Text(
-                            "Today",
+                            DateFormat('EEEE').format(day1),
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                           Text(
-                            "24°/12°",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Icon(
-                            Icons.sunny,
-                            color: Colors.yellow,
-                          ),
-                          Text(
-                            "Today",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          Text(
-                            "24°/12°",
+                            "$maxTemperatureDay1°/$minTemperatureDay1°",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
@@ -133,11 +175,31 @@ class HomeScreenState extends State<HomeScreen> {
                             color: Colors.yellow,
                           ),
                           Text(
-                            "Today",
+                            DateFormat('EEEE').format(day2),
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                           Text(
-                            "24°/12°",
+                            "$maxTemperatureDay2°/$minTemperatureDay2°",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.sunny,
+                            color: Colors.yellow,
+                          ),
+                          Text(
+                            DateFormat('EEEE').format(day3),
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          Text(
+                            "$maxTemperatureDay3°/$minTemperatureDay3°",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ],
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,17 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:weather_application/services/location.dart';
 import 'home_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
-
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late String cityName;
   @override
   void initState() {
     super.initState();
@@ -23,31 +25,43 @@ class _LoadingScreenState extends State<LoadingScreen> {
     double longitude = lon;
     String appId = '8d481789260fee5c79b2b1d513c85944';
     String url =
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$appId';
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$appId&units=metric';
     Uri uri = Uri.parse(url);
     http.Response res = await http.get(uri);
     if (res.statusCode == 200) {
-      String data = res.body;
-      return jsonDecode(data);
+      return jsonDecode(res.body);
     } else {
       return res.statusCode.toString();
     }
   }
 
-  Future getLocationDataFromAccWeather(lat, lon) async {
-    double latitude = lat;
-    double longitude = lon;
-    String appId = '44Tcy7AhVjpQ8wxKoUAoGFzaG4mcyMyy';
+  Future getCityId(lat, lon, appId) async {
     String urlFroCityId =
-        'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=$appId&q=$latitude%2C$longitude';
+        'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=$appId&q=$lat%2C$lon';
     Uri uriForCityId = Uri.parse(urlFroCityId);
     http.Response resForCityId = await http.get(uriForCityId);
-    String cityId = jsonDecode(resForCityId.body)["Key"];
+    cityName = jsonDecode(resForCityId.body)['EnglishName'];
+    return jsonDecode(resForCityId.body)["Key"];
+  }
+
+  Future getCurrentLocationDataFromAccWeather(lat, lon) async {
+    String appId = '44Tcy7AhVjpQ8wxKoUAoGFzaG4mcyMyy';
+    String cityId = await getCityId(lat, lon, appId);
     String url =
         'http://dataservice.accuweather.com/currentconditions/v1/$cityId?apikey=$appId';
     Uri uri = Uri.parse(url);
     http.Response res = await http.get(uri);
-    return res.body;
+    return jsonDecode(res.body);
+  }
+
+  Future get3DaysForecastFromAccWeather(lat, lon) async {
+    String appId = '44Tcy7AhVjpQ8wxKoUAoGFzaG4mcyMyy';
+    String cityId = await getCityId(lat, lon, appId);
+    String url =
+        'http://dataservice.accuweather.com/forecasts/v1/daily/5day/$cityId?apikey=$appId&metric=true';
+    Uri uri = Uri.parse(url);
+    http.Response res = await http.get(uri);
+    return jsonDecode(res.body);
   }
 
   void getLocationData() async {
@@ -55,12 +69,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     double latitude = location.latitude;
     double longitude = location.longitude;
-    Future dataFromOpenWeatherMap =
-        getLocationDataFromOpenWeatherMap(latitude, longitude);
-    Future dataFromAccWeather =
-        getLocationDataFromAccWeather(latitude, longitude);
+    var dataFromOpenWeatherMap =
+        await getLocationDataFromOpenWeatherMap(latitude, longitude);
+    var currentDataFromAccWeather =
+        await getCurrentLocationDataFromAccWeather(latitude, longitude);
+    var dataFromAccWeather =
+        await get3DaysForecastFromAccWeather(latitude, longitude);
     // print(await dataFromOpenWeatherMap);
-    print(await dataFromAccWeather);
 
     // String appId = '8d481789260fee5c79b2b1d513c85944';
     // String url =
@@ -76,12 +91,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // }
     // ignore: use_build_context_synchronously
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return const HomeScreen();
+      return HomeScreen(
+        currentLocationWeather: currentDataFromAccWeather,
+        LocationWeather: dataFromAccWeather,
+        cityName: cityName,
+      );
     }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Color(0xFF039BE5),
+      body: Center(
+        child: SpinKitThreeBounce(
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
+    );
   }
 }
